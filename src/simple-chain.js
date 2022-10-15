eefc985fb779c9d343075c17271f074081b9f971
Serafim <7620226@gmail.com>
2022-10-15 17:50:14 +0300

    feat: chain task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,32 +1,58 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Implement chainMaker object according to task description
- * 
+ *
  */
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let itemsAmount = this.chain.filter((item) => item != "~~").length;
+    return itemsAmount;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value = "( )") {
+    if (this.chain.length == 0) {
+      this.chain.push(`( ${value} )`);
+    } else {
+      this.chain.push("~~");
+      this.chain.push(`( ${value} )`);
+    }
+
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if (
+      !isFinite(position) ||
+      !Number.isInteger(position) ||
+      position > this.getLength() ||
+      position <= 0
+    ) {
+      throw new Error("You can't remove incorrect link!");
+    }
+
+    let chainIndex = position - 1;
+    let items = this.chain.filter((item) => item != "~~");
+    items.splice(chainIndex, 1);
+
+    this.chain = [];
+    for (let item of items) {
+      this.chain.push(item);
+      this.chain.push("~~");
+    }
+    this.chain.splice(-1);
+    return this;
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    let copyChain = this.chain.slice();
+    this.chain = [];
+    return copyChain.join("");
+  },
 };
 
 module.exports = {
-  chainMaker
+  chainMaker,
 };
